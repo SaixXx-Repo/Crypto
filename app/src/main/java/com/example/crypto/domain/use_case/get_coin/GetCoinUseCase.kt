f0bc155b104eb712e1b20b54a013cb557921e03c
SaixXx_Desktop <maxfunk@ymail.com>
2021-09-22 17:15:42 +0200

    Create use cases for getting list of coins and for getting details of single coin

---
+++ app/src/main/java/com/example/crypto/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -0,0 +1,30 @@
+package com.example.crypto.domain.use_case.get_coin
+
+import com.example.crypto.common.Resource
+import com.example.crypto.data.remote.dto.toCoin
+import com.example.crypto.data.remote.dto.toCoinDetail
+import com.example.crypto.domain.model.Coin
+import com.example.crypto.domain.model.CoinDetail
+import com.example.crypto.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+
+    operator fun invoke(coinId: String) : Flow<Resource<CoinDetail>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coin = repository.getCoinById(coinId).toCoinDetail()
+            emit(Resource.Success(coin))
+        }catch (e: HttpException){ // Error Response from API
+            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured"))
+        }catch (e: IOException){ // No connection to remote API, e.g. no Internet connection
+            emit(Resource.Error("No internet connection"))
+        }
+    }
+}
